89f7b4e5d1ebe22a6bbe75b78f9416c1949c5900
sqz777In91 <sqz777isme@gmail.com>
2017-12-31 17:56:38 +0800

    加入專案檔案。

---
+++ Codewars_GetTheMiddleCharacter/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codewars_GetTheMiddleCharacter
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
